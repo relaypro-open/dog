f4157bdeaf93247e27d985dafaa5df20d8bc7124
Drew Gulino <dgulino@republicwireless.com>
2021-02-16 18:16:15 -0500

    separate mac install script

---
+++ install_dog_in_a_box-mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+vagrant plugin install vagrant-proxyconf
+
+# virtualbox
+vagrant plugin install vagrant-vbguest
+
+vagrant up
+
+vagrant ssh-config | tee -a ~/.ssh/config
+
+ssh-add .vagrant/machines/dog-vm-host/virtualbox/private_key
+
+echo "provisioning script completed."
+
+echo "how to connect: 
+vagrant ssh 
+or
+ssh dog-vm-host"
+
+ssh vagrant@dog-vm-host "/ansible/run_ansible.sh"
