662abd965716e2be775225ded526190000886929
Drew Gulino <dgulino@republicwireless.com>
2021-02-16 21:10:31 -0500

    asdf install from git

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -16,11 +16,18 @@ lxc file push /home/vagrant/.ssh/authorized_keys dog-agent2/root/.ssh/authorized
 
 cd /ansible
 
+sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libpng-dev libssh-dev xsltproc libxml2-utils libncurses-dev unzip
 #cp -r /ansible/vagrant_dot_asdf /home/vagrant/.asdf
 #sudo cp /ansible/vagrant_profile-d_asdf_sh /etc/profile.d/asdf.sh
-. /etc/profile
-. /etc/bash.bashrc
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+echo ". $HOME/.asdf/asdf.sh" >> ~/.bash_profile
+echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bash_profile
+. ~/.bash_profile
+asdf plugin add erlang
+asdf plugin add elixir
+asdf install erlang 22.3.2
 asdf global erlang 22.3.2
+asdf install elixir 1.9.4
 asdf global elixir 1.9.4
 asdf current
 
