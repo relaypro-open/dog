36679c602f8b91faa18896ff9cc89b31d78aa494
Drew Gulino <dgulino@relaypro.com>
2022-07-27 11:40:25 -0400

    script to build and start a test dog system via docker compose

---
+++ dog_on_a_dock.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+git clone https://github.com/relaypro-open/dog_agent.git
+cd dog_agent
+git pull
+git checkout feature/ansible_connection
+cd ..
+
+git clone https://github.com/relaypro-open/dog_trainer.git
+cd dog_trainer
+git pull
+git checkout feature/ansible_connection
+cd ..
+
+git clone https://github.com/relaypro-open/dog_park.git
+cd dog_park
+git pull
+cd ..
+
+docker compose build
+docker compose up
