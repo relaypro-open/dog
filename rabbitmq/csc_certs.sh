3897087f170f8d157eab5629c5813eb9b8dfc8b1
Drew Gulino <dgulino@relaypro.com>
2022-11-29 14:51:45 -0500

    now using passkey to request certs along with hostkey

--- rabbitmq/csc_certs.sh
+++ rabbitmq/csc_certs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-certs=$(curl -s -d '{"fqdn": "rabbitmq"}' http://csc:9001/2015-03-31/functions/myfunction/invocations)
+passkey=$(curl -s http://csc:8000/csc/register | jq -r .passkey)
+certs=$(curl -s -d '{"fqdn": "rabbitmq", "passkey": "'$passkey'"}' http://csc:8000/csc/cert)
 echo $certs | jq -r .server_key > /etc/rabbitmq/private/server.key
 echo $certs | jq -r .server_crt > /etc/rabbitmq/certs/server.crt
 echo $certs | jq -r .ca_crt >  /etc/rabbitmq/certs/ca.crt
