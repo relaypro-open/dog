998a3b6e449248b0fe0348a95bc885511d6e72dc
Drew Gulino <dgulino@relaypro.com>
2023-01-02 20:31:20 -0500

    csc request loops until 200

--- rabbitmq/csc_certs.sh
+++ rabbitmq/csc_certs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+HTTPD=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "http://csc:8000/csc/register" -o /dev/null`
+until [ "$HTTPD" == "200" ]; do
+    printf '.'
+    sleep 3
+    HTTPD=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "http://csc:8000/csc/register" -o /dev/null`
+done
+
 passkey=$(curl -s http://csc:8000/csc/register | jq -r .passkey)
 certs=$(curl -s -d '{"fqdn": "rabbitmq", "passkey": "'$passkey'"}' http://csc:8000/csc/cert)
 echo $certs | jq -r .server_key > /etc/rabbitmq/private/server.key
