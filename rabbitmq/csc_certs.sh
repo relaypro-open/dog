b10dcae979cb059e390fdd785407bb5d0cc4bc67
Drew Gulino <dgulino@relaypro.com>
2023-01-06 15:21:57 -0500

    changed csc port

--- rabbitmq/csc_certs.sh
+++ rabbitmq/csc_certs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-HTTPD=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "http://csc:8000/csc/register" -o /dev/null`
+HTTPD=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "http://csc:9000/csc/register" -o /dev/null`
 until [ "$HTTPD" == "200" ]; do
     printf '.'
     sleep 3
-    HTTPD=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "http://csc:8000/csc/register" -o /dev/null`
+    HTTPD=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "http://csc:9000/csc/register" -o /dev/null`
 done
 
-passkey=$(curl -s http://csc:8000/csc/register | jq -r .passkey)
-certs=$(curl -s -d '{"fqdn": "rabbitmq", "passkey": "'$passkey'"}' http://csc:8000/csc/cert)
+passkey=$(curl -s http://csc:9000/csc/register | jq -r .passkey)
+certs=$(curl -s -d '{"fqdn": "rabbitmq", "passkey": "'$passkey'"}' http://csc:9000/csc/cert)
 echo $certs | jq -r .server_key > /etc/rabbitmq/private/server.key
 echo $certs | jq -r .server_crt > /etc/rabbitmq/certs/server.crt
 echo $certs | jq -r .ca_crt >  /etc/rabbitmq/certs/ca.crt
