64a12dcbeb8a1703c44625de82f76f3df61a5aaf
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2023-06-14 15:54:01 -0400

    query with curl instead of httpie

--- control/ansible/inventory_host.sh
+++ control/ansible/inventory_host.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-NAME=$(http kong:8000/api/V2/hosts APIKEY:guest | jq -r .[0].name)
+NAME=$(curl -s kong:8000/api/V2/hosts -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwZDY3NGRhNGFjNzYxMWVkYThmNzhmNzdmYTUyMWI2ZSJ9.PASNsZCTJihOO6ffBO2g-7N-OQWVI5KsnP7bq5Pi3aE" | jq -r .[0].name) 
+echo "Fist host found: $NAME"
 ansible-inventory -i environments/docker/ --host $NAME
