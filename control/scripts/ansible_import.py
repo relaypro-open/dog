7629be4c55b1267be98be96100e54065811f7fcc
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2023-06-16 16:52:44 -0400

    ansible_import.py feature complete

--- control/scripts/ansible_import.py
+++ control/scripts/ansible_import.py
@@ -4,12 +4,14 @@ import sys
 import subprocess
 import configparser
 from yaml import load
+from urllib.parse import quote_plus
 try:
     from yaml import CLoader as Loader
 except ImportError:
     from yaml import Loader
 import apiclient
 import dog.api as dc
+import difflib
 
 ansible_basedir = "/home/dgulino/Documents/workspace/playbyplay"
 dog_url = "https://dog-qa.relaydev.sh:8443/api/V2"
@@ -19,8 +21,61 @@ ansible_envs = ["mob_qa", "beta_qa", "stage.qa"]
 # dog_url = "http://kong:8000/api/V2"
 # dog_env = "docker"
 # ansible_envs = ["docker"]
-hosts = {}
-groups = {}
+ansible_to_dog_group_mapping_qa = {'android': "build_master",
+                                   'android_build_node': "build_slave",
+                                   'apiproxy': 'p19_apiproxy_qa',
+                                   'asterisk': 'asterisk_qa',
+                                   'authbroker': None,
+                                   'bauth': 'auth_qa',
+                                   'bauth_db': 'bauth_db_qa',
+                                   'build_master': 'build_master',
+                                   'build_slave': 'build_slave',
+                                   'build_slave_container': 'build_slave_rdu',
+                                   'ci': "build_master",
+                                   'consul': 'consul_qa',
+                                   'consul_us-east-1': 'consul_qa',
+                                   'consul_us-west-2': 'consul_qa',
+                                   'cortex': None,
+                                   'cortex_nodes': 'cortex_broker_qa',
+                                   'deep-gpu': None,
+                                   'dog': None,
+                                   'dog_test': 'dog_test',
+                                   'dog_trainer': 'dog_qa',
+                                   'ec2_jump': None,
+                                   'fabric': 'fabric_qa',
+                                   'fabric-broker': 'fabric_broker_qa',
+                                   'flexvpn': 'flexvpn_qa',
+                                   'gemini': 'gemini_qa',
+                                   'gpu-rdu': None,
+                                   'ibot-global': "ibot_beta_qa",
+                                   'ibot-rdu': None,
+                                   'ibot_backup': 'ibot_backup_qa',
+                                   'ibot_broker': 'ibot_broker_qa',
+                                   'ibot_db': 'ibot_db_qa',
+                                   'ibot_fdb': 'ibot_fdb_qa',
+                                   'ibot_proxy': 'ibot_proxy_qa',
+                                   'info': 'info_qa',
+                                   'iperf': 'iperf_qa',
+                                   'ipersist': 'ipersist_qa',
+                                   'ipersist_db': 'ipersist_db_qa',
+                                   'iquery-api-global': None,
+                                   'ispeech': 'ispeech_qa',
+                                   'ispeech_proxy': 'ispeech_qa',
+                                   'iupload': 'iupload_qa',
+                                   'keycloak': 'keycloak_qa',
+                                   'kibana': None,
+                                   'kiwi': None,
+                                   'local': None,
+                                   'logger': 'isw_logger_qa',
+                                   'opengrok': 'opengrok_qa',
+                                   'relayprem': None,
+                                   'test': None,
+                                   'tpu': None,
+                                   'ungrouped': None,
+                                   'vpn': None,
+                                   'wiki': 'wiki_qa',
+                                   'xdoc': 'xdoc_qa'}
+group_mapping = ansible_to_dog_group_mapping_qa
 
 
 def create_client():
@@ -34,8 +89,10 @@ def create_client():
     return client
 
 
-def update_hosts(client):
+def update_hosts(client, hosts):
     for host_name, host_vars in hosts.items():
+        # if host_name != 'it-build-qa-aws01.phoneboothdev.info':
+        #     continue
         print(f"host_name: {host_name}, host_vars: {host_vars}")
         try:
             # host = client.get_host_by_hostkey(host_name) #docker
@@ -48,18 +105,38 @@ def update_hosts(client):
         host_id = host.get("id")
         print(f"host_id: {host_id}")
         host.update({"vars": host_vars})
-        #print(f"host: {host}")
+        new_hostkey = quote_plus(host.get("hostkey"))
+        host["hostkey"] = new_hostkey
+        # print(f"host: {host}")
         client.update_host(host_id, host)
-        #break
 
 
-def update_groups(client):
+def generate_rough_group_mapping(client, groups):
+    ansible_group_to_dog_group_mapping = {}
+    dog_groups = client.get_all_groups()
+    dog_group_names = [group.get('name') for group in dog_groups]
     for group_name, group_vars in groups.items():
-        if group_name == "ungrouped":
+        matches = difflib.get_close_matches(group_name, dog_group_names, n=1)
+        ansible_group_to_dog_group_mapping[group_name] = next(iter(matches), None)
+        print(f"{group_name} {matches}")
+    return ansible_group_to_dog_group_mapping
+
+
+def update_groups(client, groups):
+    for ansible_group_name, group_vars in groups.items():
+        dog_group_name = None
+        if ansible_group_name == "ungrouped":
             continue
-        print(f"group_name: {group_name}, group_vars: {group_vars}")
+        if group_vars:
+            dog_group_name = group_vars.get("dog_group", None)
+        if not dog_group_name:
+            dog_group_name = group_mapping.get(ansible_group_name)
+        if not dog_group_name:
+            continue
+        # group_name = group_name + "_qa" # munge ansible group names to match dog group names
+        print(f"ansible_group_name: {ansible_group_name}, dog_group_name: {dog_group_name}, group_vars: {group_vars}")
         try:
-            group = client.get_group_by_name(group_name)
+            group = client.get_group_by_name(dog_group_name)
         except apiclient.exceptions.ClientError as ex:
             print(ex)
             continue
@@ -73,37 +150,42 @@ def update_groups(client):
         group.update({"vars": group_vars})
         #print(f"group: {group}")
         client.update_group(group_id, group)
-        #break
+        # break
+
+
+def gather_ansible_invetory(env):
+    hosts = {}
+    groups = {}
+    inventory_export_cmd = f'cd {ansible_basedir}; ansible-inventory -i environments/{env}/hosts --list --export -y'
+    completed_process = subprocess.run(inventory_export_cmd, shell=True, check=True, capture_output=True)
+    if completed_process.returncode == 0:
+        # print(f"completed_process: {completed_process.stdout}")
+        all = load(completed_process.stdout, Loader=Loader) # print(f"all: {all}")
+        group_children = all.get("all").get("children", {})
+        # print(f"group_children: {group_children}")
+        # print(f"groups: {groups}")
+        # print(f"hosts: {hosts}")
+        for group_name, group in group_children.items():
+            # print(f"group: {group}")
+            group_hosts = group.get("hosts", {})
+            # group_vars = group.get("vars", {})
+            groups[group_name] = group.get("vars")
+            for host_name, child in group_hosts.items():
+                # print(f"child: {child}")
+                hosts[host_name] = child
+    return hosts, groups
 
 
 def main(argv, stdout, environ):
-    for env in ansible_envs:
-        inventory_export_cmd = f'cd {ansible_basedir}; ansible-inventory -i environments/{env}/hosts --list --export -y'
-        completed_process = subprocess.run(inventory_export_cmd, shell=True, check=True, capture_output=True)
-        if completed_process.returncode == 0:
-            # print(f"completed_process: {completed_process.stdout}")
-            all = load(completed_process.stdout, Loader=Loader)
-            # print(f"all: {all}")
-            group_children = all.get("all").get("children", {})
-            # print(f"group_children: {group_children}")
-            # print(f"groups: {groups}")
-            # print(f"hosts: {hosts}")
-            for group_name, group in group_children.items():
-                # print(f"group: {group}")
-                group_hosts = group.get("hosts", {})
-                # group_vars = group.get("vars", {})
-                groups[group_name] = group.get("vars")
-                for host_name, child in group_hosts.items():
-                    # print(f"child: {child}")
-                    hosts[host_name] = child
-
-    print(f'groups: {groups}')
-    print(f'hosts: {hosts}')
-
-    print("UPDATES")
     client = create_client()
-    update_hosts(client)
-    update_groups(client)
+    for env in ansible_envs:
+        hosts, groups = gather_ansible_invetory(env)
+        # update_hosts(client)
+        print(f'groups: {groups}')
+        print(f'hosts: {hosts}')
+        print("UPDATES")
+        update_hosts(client, hosts)
+        update_groups(client, groups)
 
 
 if __name__ == "__main__":
